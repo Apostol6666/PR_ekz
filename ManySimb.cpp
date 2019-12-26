d7d5b7d3fb6e9c0ec4db47352eec4821f6a61227
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2019-12-26 16:38:48 +0800

    Create ManySimb.cpp

---
+++ ManySimb.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include <iostream>
+#include "head.h"
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	ifstream on;
+	on.open("text.txt");
+
+	Russ k;
+
+
+	return 0;
+}
