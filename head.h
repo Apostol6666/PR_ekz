b2d04f1d126198a2b10aa37f3e252258304ee0ca
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2019-12-26 23:42:08 +0800

    Update head.h

--- head.h
+++ head.h
@@ -15,6 +15,7 @@ public:
 	Russ operator -(const Russ &str) const;
 	int operator >(const Russ &str);
 	Russ &ob(const Russ &str, const Russ &strtwo);
+	void addInFile(const string file_name);
 
 	void push(const char &k);
 	void remove(const char &k);
